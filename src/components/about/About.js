e7c69337be88e0ab5cad1f1fb7f3d3fa4c7f4ead
kourosh af <kouroshaf81@gmail.com>
2023-10-18 23:53:49 +0330

    add gh-pages package

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,39 +3,40 @@ import AboutCard from './AboutCard'
 import './About.css'
 import HeadTitle from '../../common/HeadTitle/HeadTitle'
 const About = () => {
-  return (
-    <>
-        <HeadTitle />
-        <section className='about top'>
-            <div className="container">
-                <AboutCard />
-            </div>
-        </section>
-
-        <section className="features top">
-            <div className="container aboutCard flex-space">
-                <div className="row row1">
-                    <h1>
-                        Our <span>Features</span>
-                    </h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cumque est magni molestiae fugit odio voluptas corporis, aliquam adipisci ratione officia! Perspiciatis quam placeat eos accusantium, saepe possimus alias debitis!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cumque est magni molestiae fugit odio voluptas corporis, aliquam adipisci ratione officia! Perspiciatis quam placeat eos accusantium, saepe possimus alias debitis!</p>
-                    <button className="secondary-btn">
-                        Explore More <i className="fas fa-long-arrow-alt-right"></i>   
-                    </button>    
+    return (
+        <>
+            <HeadTitle />
+            <section className='about top'>
+                <div className="container">
+                    <AboutCard />
                 </div>
-                <div className="row image">
-                    <img src="/images/feature-img-1.jpg" alt="" style={{'margin-top': '10px'}}/>
-                    <div className="control-btn">
-                        <button className="prev">
-                            <i className="fas fa-play"></i>
+            </section>
+
+            <section className="features top">
+                <div className="container aboutCard flex-space">
+                    <div className="row row1">
+                        <h1>
+                            Our <span>Features</span>
+                        </h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Eumcumque est magni molestiae
+                            fugit odio voluptas corporis, aliquam adipisci ratione officia! Perspiciatis quam placeat eos accusantium, saepe possimus alias debitis!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cumque est magni molestiae fugit odio voluptas corporis, aliquam adipisci ratione officia! Perspiciatis quam placeat eos accusantium, saepe possimus alias debitis!</p>
+                        <button className="secondary-btn">
+                            Explore More <i className="fas fa-long-arrow-alt-right"></i>
                         </button>
                     </div>
+                    <div className="row image">
+                        <img src="/images/feature-img-1.jpg" alt="" style={{ 'margin-top': '10px' }} />
+                        <div className="control-btn">
+                            <button className="prev">
+                                <i className="fas fa-play"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </section>
-    </>
-  )
+            </section>
+        </>
+    )
 }
 
 export default About
